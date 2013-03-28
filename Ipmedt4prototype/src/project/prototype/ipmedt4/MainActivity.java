015dfcec6321c73a73b12d1cccb70afb671ee023
Jim Schoorl <helldownthere360@gmail.com>
2013-03-28 14:01:01 +0100

    r.layout.main -> main cannot be resolved or is not a field Added code to try to connect to local MySQL server.

--- Ipmedt4prototype/src/project/prototype/ipmedt4/MainActivity.java
+++ Ipmedt4prototype/src/project/prototype/ipmedt4/MainActivity.java
@@ -1,24 +1,48 @@
 package project.prototype.ipmedt4;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		//asdfasdf
+	String str="new";
+    static ResultSet rs;
+    static PreparedStatement st;
+    static Connection con;
 
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        final   TextView tv=(TextView)findViewById(R.id.user);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+              con=DriverManager.getConnection("jdbc:mysql://10.0.2.2:8080/example","root","");
+            st=con.prepareStatement("select * from country where id=1");
+            rs=st.executeQuery();
+             while(rs.next())
+             {
+             str=rs.getString(2);
 
+
+             }
+
+
+            tv.setText(str);
+            setContentView(tv);
+        }
+        catch(Exception e)
+        {
+            tv.setText(str);
+        }
+    }
 }
