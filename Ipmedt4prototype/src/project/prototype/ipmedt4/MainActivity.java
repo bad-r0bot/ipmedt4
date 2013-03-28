0a9980196cce04e50093dfaa5cebd1ae90b9b0b7
Lnoorlander <larsnoorlander@msn.com>
2013-03-28 09:17:57 +0100

    HALLO IK BEN LARS

--- Ipmedt4prototype/src/project/prototype/ipmedt4/MainActivity.java
+++ Ipmedt4prototype/src/project/prototype/ipmedt4/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		//asdfasdf
+		//asdfasdf GA IS EVEN WEG 
 
 	}
 
