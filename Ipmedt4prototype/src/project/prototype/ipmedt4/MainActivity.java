d94887c99a4aa0c72ba91864031aecc2306b0235
Jim Schoorl <helldownthere360@gmail.com>
2013-03-28 14:14:58 +0100

    Activity en Main fout goed gekregen.

--- Ipmedt4prototype/src/project/prototype/ipmedt4/MainActivity.java
+++ Ipmedt4prototype/src/project/prototype/ipmedt4/MainActivity.java
@@ -1,14 +1,13 @@
 package project.prototype.ipmedt4;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.widget.TextView;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -20,13 +19,13 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
         final   TextView tv=(TextView)findViewById(R.id.user);
 
         try
         {
             Class.forName("com.mysql.jdbc.Driver");
-              con=DriverManager.getConnection("jdbc:mysql://10.0.2.2:8080/example","root","");
+              con=DriverManager.getConnection("jdbc:mysql://10.0.2.2:444/","root","");
             st=con.prepareStatement("select * from country where id=1");
             rs=st.executeQuery();
              while(rs.next())
