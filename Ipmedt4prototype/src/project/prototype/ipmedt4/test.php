69c8e18fb6fa63919288a03251cc96275d92141e
Jim Schoorl <hellodownthere360@gmail.com>
2013-04-22 14:18:55 +0200

    Een commit om de, tot nu toe, werkende versie te hebben.

--- Ipmedt4prototype/src/project/prototype/ipmedt4/test.php
+++ Ipmedt4prototype/src/project/prototype/ipmedt4/test.php
@@ -1,7 +1,7 @@
   <?php
 $link = mysqli_connect('localhost', 'root', '', 'movedb');
 
-    $query = "SELECT Attraction_name_Dutch, Attraction_description_Dutch FROM attractions";
+    $query = "SELECT Attraction_ID, Attraction_name_Dutch, Attraction_description_Dutch FROM attractions WHERE Attraction_ID <=6 ORDER BY Attraction_ID ASC";
 
     $result = mysqli_query($link, $query) or die(mysqli_error($link));
 
